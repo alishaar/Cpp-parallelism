57dccd03491f08d560ad8390dfd4d7f1b38374e7
alishaar <45029495+alishaar@users.noreply.github.com>
2021-01-15 08:42:48 -0500

    Delete distribution.cc

--- distribution.cc
+++
@@ -1,9 +0,0 @@
-#include "distribution.h"
-
-//distribution function definition
-
-#pragma omp declare simd
-float dist_func(const float alpha, const float rn) {
-  return delta_max*sinf(alpha*rn)*expf(-rn*rn);
-
-}
